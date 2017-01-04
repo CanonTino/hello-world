57b339b6375e6e110ff640e76c1cc825173965f6
Tino Salaz <florentini.salaz@gmail.com>
2017-01-04 16:03:49 -0600

    Initial commit

---
+++ src/main/java/net/tecgurus/app/App.java
@@ -0,0 +1,15 @@
+package net.tecgurus.app;
+
+import net.tecgurus.model.User;
+
+public class App {
+
+	public static void main(String[] args) {
+		User user = new User();
+		user.setFirstName("Florentino");
+		user.setLastName("Salazar");
+		
+		System.out.println("Created User: " + user.getFullName());
+	}
+
+}
